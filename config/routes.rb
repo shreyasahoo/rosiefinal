84e8c9f26e584e5a383b3fddb66afd078f39b7ef
shreyasahoo <shreyasahoo@berkeley.edu>
2018-01-24 22:48:05 -0800

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  root 'welcome#index'
   resources :journals
+
+
+  authenticated :user do
+  	root 'journals#index', as: "authenticated_root"
+  end
+  
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
